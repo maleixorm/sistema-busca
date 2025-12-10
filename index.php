41cc68c602cab795af3171a3d0c64657ee4cbd30
Marcos Aleixo <maknytz@gmail.com>
2025-12-10 14:51:33 -0300

    Criando o formulário do sistema de busca

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Busca</title>
+</head>
+<body>
+    <h1>Lista de Veículos</h1>
+    <form action="" method="post">
+        <input type="text" name="busca" placeholder="Digite os termos de pesquisa...">
+        <button type="submit">Pesquisar</button>
+    </form>
+    <table border="1" width="400px">
+        <tr>
+            <th>Marca</th>
+            <th>Veículo</th>
+            <th>Modelo</th>
+        </tr>
+        <tr>
+            <td colspan="3">Nada foi pesquisado até o momento...</td>
+        </tr>
+    </table>
+</body>
+</html>
