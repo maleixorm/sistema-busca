8a761f7762293ebf9adca5b81426c240dff064bb
Marcos Aleixo <maknytz@gmail.com>
2025-12-12 10:19:59 -0300

    Criando o sistema que busca no banco de dados

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php 
     include("conexao.php");
-    $pesquisa = isset($_GET['busca']);
-    $sql = "SELECT * FROM veiculos WHERE fabricante LIKE '%$pesquisa%' OR modelo LIKE '%$pesquisa%' OR veiculo LIKE '%$pesquisa%'";
 ?>
 
 <!DOCTYPE html>
@@ -13,7 +11,7 @@
 </head>
 <body>
     <h1>Lista de Veículos</h1>
-    <form action="" method="post">
+    <form action="" method="get">
         <input type="text" name="busca" placeholder="Digite os termos de pesquisa...">
         <button type="submit">Pesquisar</button>
     </form>
@@ -23,9 +21,31 @@
             <th>Veículo</th>
             <th>Modelo</th>
         </tr>
-        <tr>
-            <td colspan="3">Nada foi pesquisado até o momento...</td>
-        </tr>
+        <?php
+        if (!isset($_GET['busca'])) { ?>
+            <tr>
+                <td colspan="3">Nada foi pesquisado até o momento...</td>
+            </tr>
+        <?php } else {
+            $pesquisa = $mysqli->real_escape_string($_GET['busca']);
+            $sql = "SELECT * FROM veiculos WHERE marca LIKE '%$pesquisa%' OR modelo LIKE '%$pesquisa%' OR veiculo LIKE '%$pesquisa%'";
+            $query = $mysqli->query($sql) or die("Erro ao consultar o banco de dados: " . $mysqli->error);
+            if ($query->num_rows == 0) { ?>
+                <tr>
+                    <td colspan="3">Nenhum resultado encontrado...</td>
+                </tr>
+            <?php } else { 
+                while ($dados = $query->fetch_assoc()) { ?>
+                    <tr>
+                        <td><?= $dados['marca'] ?></td>
+                        <td><?= $dados['modelo'] ?></td>
+                        <td><?= $dados['ano'] ?></td>
+                    </tr>
+                <?php }
+                ?>
+                
+            <?php } ?>    
+        <?php } ?>
     </table>
 </body>
 </html>
